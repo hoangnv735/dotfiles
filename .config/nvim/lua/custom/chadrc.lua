1cf880c6552900fe496ad9e67b1bfbfdf707ca97
hoangnv735 <deserteaglexiii@gmail.com>
2024-03-17 17:59:08 +0700

    add tridactyl

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,9 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {
+ theme = 'one_light',
+ theme_toggle = {'one_light', 'catppuccin'}
+}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
