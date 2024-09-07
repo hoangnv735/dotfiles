3bf60003c8d1ddcbdf4f1803ee5fa87c983bf8d8
hoangnv735 <deserteaglexiii@gmail.com>
2024-09-07 10:50:41 +0700

    edit nvim config

--- nvim/lua/custom/chadrc.lua
+++
@@ -1,9 +0,0 @@
----@type ChadrcConfig 
-local M = {}
-M.ui = {
- theme = 'github_light',
- theme_toggle = {'github_light', 'github_light'}
-}
-M.plugins = "custom.plugins"
-M.mappings = require "custom.mappings"
-return M
--- nvim/lua/custom/init.lua
+++
@@ -1,2 +0,0 @@
-vim.opt.hlsearch = false
-vim.wo.relativenumber = true
